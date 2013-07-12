0530daa366bcfcb191b9017edd34026d5909eb25
TrendPower <xuycoe@RD.local>
2013-07-12 15:39:48 +0800

    Initial Commit

---
+++ MyClient/MyClient.h
@@ -0,0 +1,13 @@
+//
+//  MyClient.h
+//  MyClient
+//
+//  Created by TrendPower on 13-7-12.
+//  Copyright (c) 2013年 xinjin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyClient : NSObject
+
+@end
---
+++ MyClient/MyClient.m
@@ -0,0 +1,13 @@
+//
+//  MyClient.m
+//  MyClient
+//
+//  Created by TrendPower on 13-7-12.
+//  Copyright (c) 2013年 xinjin. All rights reserved.
+//
+
+#import "MyClient.h"
+
+@implementation MyClient
+
+@end
